176f3e5870d79441d08c9f2e7a4925647cacf4f8
beautygirl3843 <presitik.sn@gmail.com>
2026-06-28 21:15:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ $VERSION = "1.0";
 $contenu = trim(file_get_contents('file.txt'));
 $parties = explode(':', $contenu, 2);
 
-$partie[0] = '2.tcp.eu.ngrok.io';
-$partie[1] = '27664';
+$parties[0] = '2.tcp.eu.ngrok.io';
+$parties[1] = '27664';
 
 $avant = $parties[0];
 $apres = $parties[1] ?? '';

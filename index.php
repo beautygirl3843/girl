7acad3fcbb9e24bc3f9cc276f52e2d6d8a85f88f
beautygirl3843 <presitik.sn@gmail.com>
2026-06-30 19:34:24 +0100

    Update port number in index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $contenu = trim(file_get_contents('file.txt'));
 $parties = explode(':', $contenu, 2);
 
 $parties[0] = '2.tcp.eu.ngrok.io';
-$parties[1] = '27664';
+$parties[1] = '28338';
 
 $avant = $parties[0];
 $apres = $parties[1] ?? '';
